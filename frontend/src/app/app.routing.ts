7ccd3c13924da89dfbdd7866390b05a3e3f1ce9e
ldrkic1 <ldrkic1@etf.unsa.ba>
2021-05-16 22:40:00 +0200

    registracija ui

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home';
 import { VehicleComponent } from './vehicle';
 import { ReviewComponent } from './review';
 import { FaqComponent } from './faq';
+import { SignUpComponent } from './signup';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'vehicle', component: VehicleComponent },
     { path: 'review', component: ReviewComponent },
     { path: 'faq', component: FaqComponent },
+    { path: 'signup', component: SignUpComponent},
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
