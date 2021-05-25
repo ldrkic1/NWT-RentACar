5a0720a8b5781163fbcbacefb44b7047ebf5f2b1
idedic2 <idedic2@etf.unsa.ba>
2021-05-25 18:54:52 +0200

    search

--- frontend/src/app/AdminReview/adminReview.component.ts
+++ frontend/src/app/AdminReview/adminReview.component.ts
@@ -4,8 +4,7 @@ import { AdminReviewService } from './adminReview.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 import { ReviewService } from '../review/review.service';
-
-
+import { User } from '../models/User';
 
 @Component({ 
     templateUrl: 'adminReview.component.html',
@@ -46,6 +45,24 @@ export class AdminReviewComponent implements OnInit{
             this.getReviews();
           }
         );
+      }
+
+      public searchClients(key: string): void {
+        console.log(key);
+        const results: Review[] = [];
+        for (const review of this.reviews) {
+          if (review.user.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || review.user.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || review.user.username.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          || review.title.toLowerCase().indexOf(key.toLowerCase()) !== -1
+          ) {
+            results.push(review);
+          }
+        }
+        this.reviews = results;
+        if (results.length === 0 || !key) {
+          this.getReviews();
+        }
       }
 
 }
