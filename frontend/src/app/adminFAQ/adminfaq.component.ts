6b1111290887f60cb4352527e0547143e910aa89
ldrkic1 <ldrkic1@etf.unsa.ba>
2021-05-25 19:01:59 +0200

    css izmjene

--- frontend/src/app/adminFAQ/adminfaq.component.ts
+++ frontend/src/app/adminFAQ/adminfaq.component.ts
@@ -29,6 +29,24 @@ export class AdminFaqComponent implements OnInit {
         );
     }
 
+    public searchClients(key: string): void {
+      console.log(key);
+      const results: Question[] = [];
+      for (const question of this.questions) {
+        if (question.user.firstName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || question.user.lastName.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || question.user.username.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || question.title.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        ) {
+          results.push(question);
+        }
+      }
+      this.questions = results;
+      if (results.length === 0 || !key) {
+        this.getQuestions();
+      }
+    }
+
     public onDeleteQuestion(questionId: number): void {
         this.questionService.deleteQuestion(questionId).subscribe(
           (response: any) => {
